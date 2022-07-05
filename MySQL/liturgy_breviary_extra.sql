06e17eff731ea72701b37a4d304376cb2b590b50
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2022-07-05 21:57:13 +0530

    Create liturgy_breviary_extra.sql

---
+++ MySQL/liturgy_breviary_extra.sql
@@ -0,0 +1,22 @@
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `morning`
+--
+ALTER TABLE `morning`
+  ADD PRIMARY KEY (`dayID`);
+
+--
+-- Indexes for table `psalter`
+--
+ALTER TABLE `psalter`
+  ADD PRIMARY KEY (`dayID`);
+
+--
+-- Indexes for table `text`
+--
+ALTER TABLE `text`
+  ADD PRIMARY KEY (`refKey`);
