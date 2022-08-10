2a8956a919e0cd34a174b753bc92cd0dc1c88ee7
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2022-08-10 22:47:23 +0530

    restructure DB

--- MySQL/liturgy_breviary_extra.sql
+++ MySQL/liturgy_breviary_extra.sql
@@ -10,13 +10,31 @@ ALTER TABLE `proper`
   ADD PRIMARY KEY (`dayID`);
 
 --
+-- Indexes for table `proper_sunday`
+--
+ALTER TABLE `proper_sunday`
+  ADD PRIMARY KEY (`refKey`);
+
+--
 -- Indexes for table `psalter`
 --
 ALTER TABLE `psalter`
-  ADD PRIMARY KEY (`dayID`);
+  ADD PRIMARY KEY (`dayID`,`Ps_no`);
 
 --
--- Indexes for table `text`
+-- Indexes for table `text__concludingprayer`
 --
-ALTER TABLE `text`
+ALTER TABLE `text__concludingprayer`
+  ADD PRIMARY KEY (`refKey`);
+
+--
+-- Indexes for table `text__hymns`
+--
+ALTER TABLE `text__hymns`
+  ADD PRIMARY KEY (`refKey`);
+
+--
+-- Indexes for table `text__scripture`
+--
+ALTER TABLE `text__scripture`
   ADD PRIMARY KEY (`refKey`);
