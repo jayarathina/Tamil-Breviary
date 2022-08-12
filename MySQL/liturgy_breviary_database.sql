fd9b116038abfeff01a807d776ad38620e06c6e8
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2022-08-12 12:19:17 +0530

    restructure DB

--- MySQL/liturgy_breviary_database.sql
+++ MySQL/liturgy_breviary_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 09, 2022 at 07:45 PM
+-- Generation Time: Aug 12, 2022 at 08:43 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
--- MySQL/liturgy_breviary_extra.sql
+++ MySQL/liturgy_breviary_extra.sql
@@ -4,16 +4,34 @@
 --
 
 --
--- Indexes for table `proper`
+-- Indexes for table `proper__concludingprayer`
 --
-ALTER TABLE `proper`
+ALTER TABLE `proper__concludingprayer`
   ADD PRIMARY KEY (`dayID`);
 
 --
--- Indexes for table `proper_sunday`
+-- Indexes for table `proper__gsc_antiphon`
 --
-ALTER TABLE `proper_sunday`
-  ADD PRIMARY KEY (`refKey`);
+ALTER TABLE `proper__gsc_antiphon`
+  ADD PRIMARY KEY (`dayID`);
+
+--
+-- Indexes for table `proper__intercessions`
+--
+ALTER TABLE `proper__intercessions`
+  ADD PRIMARY KEY (`dayID`);
+
+--
+-- Indexes for table `proper__scripture`
+--
+ALTER TABLE `proper__scripture`
+  ADD PRIMARY KEY (`dayID`);
+
+--
+-- Indexes for table `proper__s_resp`
+--
+ALTER TABLE `proper__s_resp`
+  ADD PRIMARY KEY (`dayID`);
 
 --
 -- Indexes for table `psalter`
@@ -22,10 +40,10 @@ ALTER TABLE `psalter`
   ADD PRIMARY KEY (`dayID`,`Ps_no`);
 
 --
--- Indexes for table `text__concludingprayer`
+-- Indexes for table `psalter__antiphon`
 --
-ALTER TABLE `text__concludingprayer`
-  ADD PRIMARY KEY (`refKey`);
+ALTER TABLE `psalter__antiphon`
+  ADD PRIMARY KEY (`dayID`,`Ps_no`);
 
 --
 -- Indexes for table `text__hymns`
