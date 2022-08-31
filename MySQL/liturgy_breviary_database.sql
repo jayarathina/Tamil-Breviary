fcec3781af902684d846f904ec10062fe620578b
Jayarathina Madharasan. Y <jayarathina@users.noreply.github.com>
2022-08-31 18:50:24 +0530

    mid-day prayer update

--- MySQL/liturgy_breviary_database.sql
+++ MySQL/liturgy_breviary_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 31, 2022 at 02:53 PM
+-- Generation Time: Aug 31, 2022 at 03:17 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
